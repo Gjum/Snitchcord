5f19ba0b4b1fa9cc264627d46daee619a394f916
Gjum <code.gjum@gmail.com>
2017-05-28 06:28:52 +0200

    add key to open gui

--- src/main/java/gjum/minecraft/forge/snitchcord/KeyHandler.java
+++ src/main/java/gjum/minecraft/forge/snitchcord/KeyHandler.java
@@ -1,6 +1,8 @@
 package gjum.minecraft.forge.snitchcord;
 
+import gjum.minecraft.forge.snitchcord.config.ConfigGui;
 import gjum.minecraft.forge.snitchcord.config.SnitchcordConfig;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
@@ -10,11 +12,13 @@ import org.lwjgl.input.Keyboard;
 
 public class KeyHandler {
     private final KeyBinding toggleEnabled = new KeyBinding("Toggle sending snitches to discord", Keyboard.KEY_NONE, SnitchcordMod.MOD_NAME);
+    private final KeyBinding openMenu = new KeyBinding("Open " + SnitchcordMod.MOD_NAME + " menu", Keyboard.KEY_NONE, SnitchcordMod.MOD_NAME);
 
     private long lastCrash = 0;
 
     public KeyHandler() {
         ClientRegistry.registerKeyBinding(toggleEnabled);
+        ClientRegistry.registerKeyBinding(openMenu);
 
         MinecraftForge.EVENT_BUS.register(this);
     }
@@ -24,6 +28,9 @@ public class KeyHandler {
         try {
             if (toggleEnabled.isPressed()) {
                 SnitchcordConfig.instance.setEnabled(!SnitchcordConfig.instance.enabled);
+            }
+            if (openMenu.isPressed()) {
+                Minecraft.getMinecraft().displayGuiScreen(new ConfigGui(null));
             }
         } catch (Exception e) {
             if (lastCrash < System.currentTimeMillis() - 5000) {
